eecd6ad58d13c97cee8f02c499d9e4718adb8c75
McCall <1059511459@qq.com>
2020-07-12 23:04:54 +0800

    deploy

---
+++ precache-manifest.98becd2bbb9bfd010433aee912b64efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932af0e42e9fab812cbe",
+    "url": "/morney-website/css/app.3f212ab9.css"
+  },
+  {
+    "revision": "f1db2f346b253ab389b00660aa407094",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "932af0e42e9fab812cbe",
+    "url": "/morney-website/js/app.219ac2d1.js"
+  },
+  {
+    "revision": "dfe2dcd70ec653f303dd",
+    "url": "/morney-website/js/chunk-vendors.a4e77477.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
